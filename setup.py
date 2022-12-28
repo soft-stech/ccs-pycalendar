f1d1293acc4da147a52ead75be0a6bf4787c9c0e
rbelash <rbelash@stsoft.ru>
2022-12-28 14:03:24 +0300

    upgrade url

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name="pycalendar",
     version="2.1",
     description="iCalendar/vCard Library",
-    url="https://github.com/apple/ccs-pycalendar",
+    url="https://github.com/stech-soft/ccs-pycalendar",
     license="Apache 2.0",
     platforms=["any"],
     package_dir={'': 'src'},
